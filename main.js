4762c0b355b129567cc20eca7e5ddd4bab09e7ff
jade <jadejung95@gmail.com>
2023-11-10 18:11:46 +0900

    currency convert

--- main.js
+++ main.js
@@ -4,26 +4,26 @@
 // 4. selecting an item from the dropdown list changes to it - clink event
 // 5. enter the amount and currency exchange occurs
 // 6. selecting an item from another dropdown list changes to that currency
-// 7. read currency units in Korean
-// 8. change the number first in the box below
+// 7. change the number first in the box below
+// 8. read currency units in Korean
 
 //use Object(객체)
 let currencyRatio = {
   USD: {
-    KRW: 1341.68, // USD to KRW
+    KRW: 1310.65, // USD to KRW
     USD: 1,
     CAD: 1.38, // USD to CAD
     unit: "달러",
   },
   KRW: {
     KRW: 1,
-    USD: 0.00075,
-    CAD: 0.001,
+    USD: 0.00076,
+    CAD: 0.0011,
     unit: "원",
   },
   CAD: {
-    KRW: 970.71,
-    USD: 0.72,
+    KRW: 950.28,
+    USD: 0.73,
     CAD: 1,
     unit: "캐드",
   },
@@ -41,6 +41,7 @@ let toCurrency = "USD";
 
 document.querySelectorAll("#from-currency-list a").forEach((menu) =>
   menu.addEventListener("click", function () {
+    document.getElementById("from-input").value = null;
     // add event 'click' to function 'menu'
     //1. Get the button.
     //2. Replace the value in the button.
@@ -49,12 +50,46 @@ document.querySelectorAll("#from-currency-list a").forEach((menu) =>
     //3. Save the selected currency value in a variable.
     fromCurrency = this.textContent;
     console.log("fromCurrency: ", fromCurrency);
+
+    document.getElementById("from-unit").textContent =
+      currencyRatio[fromCurrency].unit;
   })
 );
 
 document.querySelectorAll("#to-currency-list a").forEach((menu) =>
   menu.addEventListener("click", function () {
+    document.getElementById("to-input").value = null;
+
     document.getElementById("to-button").textContent = this.textContent;
     toCurrency = this.textContent;
+
+    document.getElementById("to-unit").textContent =
+      currencyRatio[toCurrency].unit;
   })
 );
+
+// 1. when entering key
+// 2. currency exchange occurs
+// 3. show the value
+
+function convert(type) {
+  //console.log("keyup: ");
+
+  if (type == "from") {
+    //convert
+    // currency-exchange = money * exchange rate
+    let amount = document.getElementById("from-input").value;
+    let convertedAmount = amount * currencyRatio[fromCurrency][toCurrency];
+    console.log("환전결과!:", convertedAmount);
+
+    document.getElementById("to-input").value = convertedAmount;
+  } else {
+    //convert
+    // currency-exchange = money * exchange rate
+    let amount = document.getElementById("to-input").value;
+    let convertedAmount = amount * currencyRatio[toCurrency][fromCurrency];
+    console.log("환전결과!:", convertedAmount);
+
+    document.getElementById("from-input").value = convertedAmount;
+  }
+}
